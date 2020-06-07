69c4318775e1f25c96c1b6b62da140566a3299b6
Nico Nößler <nico@streamstack.tv>
2020-06-07 20:57:18 +0200

    deleted hardcopy

---
+++ HybridEngine/main.cpp
@@ -0,0 +1,31 @@
+//
+// Created by ninoe on 07.06.2020.
+//
+#include <iostream>
+#include "GLFW/glfw3.h"
+
+int main() {
+    GLFWwindow* window;
+
+    if (!glfwInit()) {
+        return -1;
+    }
+
+    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+    if (!window) {
+        glfwTerminate();
+        return -1;
+    }
+
+    glfwMakeContextCurrent(window);
+
+    while (!glfwWindowShouldClose(window)) {
+        // Render Loop
+        glClear(GL_COLOR_BUFFER_BIT);
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+    return 0;
+}
